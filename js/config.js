735f7a3cbf2a70e16065fa624403e82daf778ba2
mar111yna <mar111yna@proton.me>
2024-10-10 12:06:55 +0200

    Update config.js with Cognito user pool credentials

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_swPpqM72q', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2hmvaqh685f926e5nha9i0f5o8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
